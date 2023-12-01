1f24f97d87aaa2316e113cf060374d6fb0cfaaef
KoichiMin <koichidminami@gmail.com>
2023-12-01 13:07:43 -0500

    test

--- src/main/java/com/pluralsight/ShoppingList.java
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -4,6 +4,7 @@ public class ShoppingList {
 
     public static void main(String[] args){
         System.out.println("Apple, grapefruit, Cheese, Orange");
+        System.out.println("Apple, grapefruit, Cheese, Orange");
     }
-
+    
 }
